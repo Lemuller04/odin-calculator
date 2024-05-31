99f0c8685ae0bcf46a184e720791514bbb8ad6ae
Lemuller04 <troublessleepy@gmail.com>
2024-05-31 13:36:52 -0300

    fix: secondNumber woudn't accept floats

--- script.js
+++ script.js
@@ -20,31 +20,35 @@ buttons.forEach((button) => button.onclick = () => {
         firstPartDefined = true;
     } else if (!firstPartDefined) {
         firstN += input;
-    } else if (isNum(input)) {
+    } else if (isNum(input) || input === ".") {
         secondN += input;
     }
 
     result.textContent = `${res}`;
 
     if (input === "=") {
-        switch (operator) {
-            case "+":
-                operationResult = add(firstN, secondN);
-                break;
-            case "-":
-                operationResult = subtract(firstN, secondN);
-                break;
-            case "*":
-                operationResult = multiply(firstN, secondN);
-                break;
-            case "/":
-                operationResult = divide(firstN, secondN);
-                break;
-        }
+        operate();
         result.textContent = `${operationResult}`;
     }
 
 });
+
+function operate() {
+    switch (operator) {
+        case "+":
+            operationResult = add(firstN, secondN);
+            break;
+        case "-":
+            operationResult = subtract(firstN, secondN);
+            break;
+        case "*":
+            operationResult = multiply(firstN, secondN);
+            break;
+        case "/":
+            operationResult = divide(firstN, secondN);
+            break;
+    }
+}
 
 function isNum(num) {
     for (let i = 0; i < num.length; i++) {
